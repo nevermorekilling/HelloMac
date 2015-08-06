d3223e27170b3ef908dcf01b96e3d0763d6aed42
陳昂 <chenang@chinnoboru-no-MacBook-Pro.local>
2015-08-07 00:44:39 +0900

    Initial Commit

---
+++ HelloMac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloMac
+//
+//  Created by 陳昂 on 15/8/7.
+//  Copyright (c) 2015年 陳昂. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
