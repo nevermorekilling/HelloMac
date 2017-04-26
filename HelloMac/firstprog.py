81d59a1860a2c67a63451f6982b33b23b143affa
ChenAng <nevermorekillingspree@gmail.com>
2017-04-26 22:06:22 +0900

    Add firstprog by python

---
+++ HelloMac/firstprog.py
@@ -0,0 +1,48 @@
+# inp = raw_input('Enter you name: ')
+# print "Hello " + inp
+
+# Test2.3
+# hours = raw_input("Enter Hours: ")
+# rate = raw_input("Enter Rate: ")
+# pay = int(hours) * float(rate)
+# print pay
+
+#Test2.4
+# width = 17
+# height =12.0
+# trueOrFalse = raw_input("You Guess What's the type of \"width/2\" :")
+# wrappedString = "<type \'" + trueOrFalse + "\'>"
+# print "wrappedString" + wrappedString
+# typeFor = type(width/2)
+# print "typeFor" + str(typeFor)
+# 
+# print type(type(width/2))
+# 
+# if wrappedString == str(typeFor):
+# 	print "Yeah,you're right"
+# else:
+# 	print "What a pity boy"
+# 
+
+#Test2.5 Converting C to F
+# cTemp = raw_input("Enter the C Temperature: ")
+# fTemp = float(cTemp) * 1.8 + 32
+# print "The Converted Temperature is: " + str(fTemp) + "F "
+
+# Test2.5 Converting C to F
+# cTemp = raw_input("Enter the C Temperature: ")
+# try:
+# 	fTemp = float(cTemp) * 1.8 + 32
+# 	print "The Converted Temperature is: " + str(fTemp) + "F "
+# except:
+# 	print "What the fuck do you want me to do Dude?"
+
+
+# import math
+# signal_power = 9
+# noise_power = 10
+# ratio = float(signal_power)/noise_power
+# decibels = 10 * (math.log10(ratio))
+# print decibels
+
+
